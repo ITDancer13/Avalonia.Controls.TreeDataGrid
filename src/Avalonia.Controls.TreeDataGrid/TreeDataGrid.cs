136cc57ef05e7e44ac07214eb9395715fc4ce1e2
Kevin Sommer <kevin@pasoma.de>
2025-04-22 07:05:46 +0200

    Revert is sorted change

--- src/Avalonia.Controls.TreeDataGrid/TreeDataGrid.cs
+++ src/Avalonia.Controls.TreeDataGrid/TreeDataGrid.cs
@@ -456,7 +456,9 @@ namespace Avalonia.Controls
             if (_source is null || RowSelection is null)
                 return;
 
-            var allowedEffects = AutoDragDropRows ? DragDropEffects.Move : DragDropEffects.None;
+            var allowedEffects = AutoDragDropRows && !_source.IsSorted ?
+                DragDropEffects.Move :
+                DragDropEffects.None;
             var route = BuildEventRoute(RowDragStartedEvent);
 
             if (route.HasHandlers)
